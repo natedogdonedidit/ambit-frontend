249c7af3bf569767a015d1afb41c8c754b1a06cc
chadwhittaker <chadwhit124@gmail.com>
2021-03-31 10:19:29 -0400

    [0.3.22] fixed small bug in TextButton. Not incrememting version.

--- src/library/components/UI/buttons/TextButton.js
+++ src/library/components/UI/buttons/TextButton.js
@@ -6,13 +6,13 @@ import { StyleSheet, TouchableOpacity, Text, View } from 'react-native';
 import colors from 'styles/colors';
 import defaultStyles from 'styles/defaultStyles';
 
-const TextButton = ({ onPress = () => null, buttonStyle, textStyle, children, disabled }) => {
+const TextButton = ({ onPress = () => null, buttonStyle, textStyle, children, disabled = false }) => {
   return (
     <TouchableOpacity
       onPress={() => onPress()}
       activeOpacity={0.5}
       hitSlop={{ top: 10, left: 10, bottom: 10, right: 10 }}
-      disabled
+      disabled={disabled}
     >
       <View style={{ ...styles.button, ...buttonStyle }}>
         <Text style={{ ...defaultStyles.defaultMedium, ...styles.buttonText, ...textStyle }}>{children}</Text>
